507228ef4189dbaf8c9fa1f5b821fdd20ba11717
CJ Longoria <curtis.longoria@gmail.com>
2023-12-11 01:37:58 -0600

    deleting 2020 from main

--- 2020/day15/src/main.rs
+++
@@ -1,7 +0,0 @@
-use std::collections::HashMap;
-use std::fs;
-
-fn main() {
-    let contents: String =
-        fs::read_to_string("data/data.txt").expect("Should have been able to read the file");
-
