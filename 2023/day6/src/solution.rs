242562d8587785d5adb1d8ace12d3e810a24d257
CJ Longoria <curtis.longoria@gmail.com>
2024-01-05 00:11:32 -0600

    day6 part 1

---
+++ 2023/day6/src/solution.rs
@@ -0,0 +1,51 @@
+pub fn part1(data: &str) {
+    let mut data_iter = data.lines();
+
+    let times = data_iter
+        .next()
+        .unwrap()
+        .strip_prefix("Time:")
+        .unwrap()
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap());
+
+    let distances = data_iter
+        .next()
+        .unwrap()
+        .strip_prefix("Distance:")
+        .unwrap()
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap());
+
+    let datapoints = times.zip(distances);
+    let ans = calculate(datapoints);
+    dbg!(ans);
+}
+
+fn calculate(datapoints: impl Iterator<Item = (i64, i64)>) -> i64 {
+    let mut start = 0;
+    let mut end = 0;
+    let mut ans = 1;
+    for datapoint in datapoints {
+        let (time, dist) = datapoint;
+        for x in 1..time {
+            if x * (time - x) > dist {
+                start = x;
+                break;
+            }
+        }
+        for x in (1..time).rev() {
+            if x * (time - x) > dist {
+                end = x;
+                break;
+            }
+        }
+        if end > 0 {
+            ans *= (end - start) + 1;
+        }
+    }
+    ans
+}
+
