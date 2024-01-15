c2f6e373bede85b3ec41b27eb19bab5ce14c41d0
CJ Longoria <curtis.longoria@gmail.com>
2024-01-14 20:34:32 -0600

    day 7 part 2

--- 2023/day7/src/solution.rs
+++ 2023/day7/src/solution.rs
@@ -17,12 +17,12 @@ struct Hand<'a> {
 }
 
 impl<'a> Hand<'a> {
-    fn new(data: &'a str) -> Self {
+    fn new(data: &'a str, joker: bool) -> Self {
         let (cards, bid_str) = data.split_once(" ").unwrap();
         let bid = bid_str.parse().unwrap();
         let map = Hand::build_map(cards);
-        let hand_type = Hand::convert_map_to_hand_type(map);
-        let sort_score = Hand::sort_score(cards);
+        let hand_type = Hand::convert_map_to_hand_type(map, joker);
+        let sort_score = Hand::sort_score(cards, joker);
         Self {
             bid,
             cards,
@@ -31,17 +31,21 @@ impl<'a> Hand<'a> {
         }
     }
 
-    fn card_score(card: &char) -> u32 {
-        "23456789TJQKA".chars().position(|x| x == *card).unwrap() as u32
+    fn card_score(card: &char, joker: bool) -> u32 {
+        if joker {
+            "J23456789TQKA".chars().position(|x| x == *card).unwrap() as u32
+        } else {
+            "23456789TJQKA".chars().position(|x| x == *card).unwrap() as u32
+        }
     }
 
-    fn sort_score(cards: &'a str) -> u32 {
+    fn sort_score(cards: &'a str, joker: bool) -> u32 {
         //We are representing the entire hand with a single number
         //by bit shifting 4 places for each card. We shift by 4 because
         //a card's value can be anywhere between 0-12
         cards
             .chars()
-            .fold(0u32, |acc, x| (acc | Hand::card_score(&x)) << 4)
+            .fold(0u32, |acc, x| (acc | Hand::card_score(&x, joker)) << 4)
     }
 
     fn build_map(cards: &'a str) -> HashMap<char, usize> {
@@ -52,9 +56,19 @@ impl<'a> Hand<'a> {
         hm
     }
 
-    fn convert_map_to_hand_type(hm: HashMap<char, usize>) -> u8 {
-        let counts: Vec<usize> = hm.into_values().collect();
-        if counts.contains(&5) {
+    fn convert_map_to_hand_type(hm: HashMap<char, usize>, joker: bool) -> u8 {
+        let joker_count = hm.get(&'J').copied().unwrap_or(0);
+        let counts: Vec<usize> = if joker {
+            hm.iter()
+                .filter(|(k, _)| **k != 'J')
+                .map(|(_, v)| v)
+                .copied()
+                .collect()
+        } else {
+            hm.into_values().collect()
+        };
+
+        let mut hand_type: u8 = if counts.contains(&5) {
             6
         } else if counts.contains(&4) {
             5
@@ -62,20 +76,40 @@ impl<'a> Hand<'a> {
             4
         } else if counts.contains(&3) {
             3
-        } else if counts.iter().filter(|x| **x == 2usize).count() == 2 {
+        } else if counts.iter().filter(|x| **x == 2usize).count() >= 2 {
             2
         } else if counts.contains(&2) {
             1
         } else {
             0
+        };
+        if joker {
+            hand_type = match (hand_type, joker_count) {
+                (_, 0) => hand_type,
+                (x, 1) if [0, 4, 5].contains(&x) => hand_type + 1,
+                (x, 1) if [1, 2, 3].contains(&x) => hand_type + 2,
+                (x, 2) if [0, 3].contains(&x) => hand_type + 3,
+                (x, 2) if [1].contains(&x) => hand_type + 4,
+                (_, 3) => hand_type + 5,
+                (_, y) if [4, 5].contains(&y) => 6,
+                _ => panic!(),
+            }
         }
+        hand_type
     }
 }
 
 pub fn part1(data: &str) {
-    let mut hands: Vec<Hand> = data.lines().map(Hand::new).collect();
+    let mut hands: Vec<Hand> = data.lines().map(|cards| Hand::new(cards, false)).collect();
     hands.sort();
     let ans: usize = hands.iter().enumerate().map(|(i, x)| (i + 1) * x.bid).sum();
     println!("Day 7 part 1 answer - {}", ans);
+}
+
+pub fn part2(data: &str) {
+    let mut hands: Vec<Hand> = data.lines().map(|cards| Hand::new(cards, true)).collect();
+    hands.sort();
+    let ans: usize = hands.iter().enumerate().map(|(i, x)| (i + 1) * x.bid).sum();
+    println!("Day 7 part 2 answer - {}", ans);
 }
 
