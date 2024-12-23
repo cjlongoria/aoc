518c2e5ae140dd450397da41ae65745993392477
cj <curtis.longoria@gmail.com>
2024-12-22 23:11:27 -0600

    day2 part2

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -1,3 +1,5 @@
+use crate::part1::part1;
+use crate::part2::part2;
 use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
@@ -24,43 +26,82 @@ fn main() {
 
     let res = match args.part {
         Part::P1 => part1(data),
-        //TODO: Change back to `part2()`
-        Part::P2 => part1(data),
+        Part::P2 => part2(data),
     };
 
     println!("ans: {}", res);
 }
 
-fn part1(data: &str) -> usize {
-    let mut count = 0;
-    for report in data.lines() {
-        let report_data: Vec<_> = report
-            .split_whitespace()
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect();
+pub mod part1 {
+    use crate::common::check;
 
-        let mut prev: Option<u32> = None;
-        let mut safe = true;
+    pub fn part1(data: &str) -> usize {
+        let mut count = 0;
+        for report in data.lines() {
+            let report_data: Vec<_> = report
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
 
-        if !report_data.is_sorted() && !report_data.iter().rev().is_sorted() {
-            continue;
+            if check(&report_data) {
+                count += 1;
+            }
+        }
+        count
+    }
+}
+
+pub mod common {
+    pub fn check(data: &[u32]) -> bool {
+        if !data.is_sorted() && !data.iter().rev().is_sorted() {
+            return false;
         }
 
-        for level in report_data {
+        let mut prev: Option<&u32> = None;
+        for level in data {
             if let Some(prev_level) = prev {
-                if !(1..=3).contains(&prev_level.abs_diff(level)) {
-                    safe = false;
-                    break;
+                if !(1..=3).contains(&prev_level.abs_diff(*level)) {
+                    return false;
                 }
             }
             prev = Some(level)
         }
-
-        if safe {
-            count += 1;
-        }
+        true
     }
-    count
 }
 
-// fn part2(data: (Vec<isize>, Vec<isize>)) -> usize {}
+pub mod part2 {
+    use crate::common::check;
+
+    // This is a pretty lame brute force approach that short circuits on the first success.
+    fn problem_dampener(data: &[u32]) -> bool {
+        let end = data.len();
+
+        for i in 0..end {
+            let left = &data[..i];
+            let right = &data[i + 1..];
+            let new = [left, right].concat();
+            if check(&new) {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn part2(data: &str) -> usize {
+        let mut count = 0;
+        for report in data.lines() {
+            let report_data: Vec<_> = report
+                .split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
+
+            if check(&report_data) {
+                count += 1;
+            } else if problem_dampener(&report_data) {
+                count += 1;
+            }
+        }
+        count
+    }
+}
