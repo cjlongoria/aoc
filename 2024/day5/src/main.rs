efdff40eb201c0ade2cad37f4145e61cd6f9859c
cj <curtis.longoria@gmail.com>
2025-01-09 23:24:41 -0600

    day5 p2

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -67,7 +67,8 @@ pub mod common {
         data: &str,
     ) -> (
         HashMap<usize, HashSet<usize>>,
-        impl Iterator<Item = impl Iterator<Item = usize> + use<'_>>,
+        Vec<Vec<usize>>,
+        // impl Iterator<Item = impl Iterator<Item = usize> + use<'_>>,
     ) {
         let (rules, updates) = split_data(data);
         let rule_map = gen_rule_map(rules);
@@ -77,23 +78,30 @@ pub mod common {
     fn split_data(
         data: &str,
     ) -> (
-        impl Iterator<Item = &str>,
-        impl Iterator<Item = impl Iterator<Item = usize> + use<'_>>,
+        Vec<&str>,
+        Vec<Vec<usize>>,
+        // impl Iterator<Item = &str>,
+        // impl Iterator<Item = impl Iterator<Item = usize> + use<'_>>,
     ) {
         data.split_once("\n\n")
             .map(|(rules, updates)| {
                 (
-                    rules.lines(),
-                    updates.lines().map(|line| {
-                        line.split(",")
-                            .map(|update| update.parse::<usize>().unwrap())
-                    }),
+                    rules.lines().collect(),
+                    updates
+                        .lines()
+                        .map(|line| {
+                            line.split(",")
+                                .map(|update| update.parse::<usize>().unwrap())
+                                .collect()
+                        })
+                        .collect(),
                 )
             })
             .unwrap()
     }
 
-    fn gen_rule_map<'a>(rules: impl Iterator<Item = &'a str>) -> HashMap<usize, HashSet<usize>> {
+    // fn gen_rule_map<'a>(rules: impl Iterator<Item = &'a str>) -> HashMap<usize, HashSet<usize>> {
+    fn gen_rule_map(rules: Vec<&str>) -> HashMap<usize, HashSet<usize>> {
         let mut map: HashMap<usize, HashSet<usize>> = HashMap::new();
         for rule in rules {
             let (key, value): (usize, usize) = rule
@@ -109,8 +117,57 @@ pub mod common {
 }
 
 pub mod part2 {
+    use std::{
+        cmp::Ordering,
+        collections::{HashMap, HashSet},
+    };
+
+    use crate::common::parse_data;
 
     pub fn part2(data: &str) -> usize {
-        0
+        let (rule_map, updates) = parse_data(data);
+
+        let mut bad_orders: Vec<Vec<usize>> = Vec::new();
+        let mut pages: Vec<usize> = Vec::new();
+        for update in updates {
+            let mut bad_order = false;
+            for page_num in update {
+                if let Some(must_come_before) = rule_map.get(&page_num) {
+                    for prev_page in &pages {
+                        if must_come_before.contains(prev_page) {
+                            bad_order = true;
+                        }
+                    }
+                }
+                pages.push(page_num);
+            }
+            if bad_order {
+                bad_orders.push(pages.clone());
+            }
+            pages.clear();
+        }
+        for order in bad_orders.iter_mut() {
+            order.sort_by(|a, b| funky_sort(a, b, &rule_map))
+        }
+        bad_orders
+            .into_iter()
+            .map(|order| order[order.len() / 2])
+            .sum()
+    }
+
+    fn funky_sort(a: &usize, b: &usize, map: &HashMap<usize, HashSet<usize>>) -> Ordering {
+        if let Some(must_come_before) = map.get(b) {
+            if must_come_before.contains(a) {
+                return Ordering::Greater;
+            }
+        };
+
+        if let Some(must_come_before) = map.get(a) {
+            if must_come_before.contains(b) {
+                return Ordering::Less;
+            }
+        };
+
+        Ordering::Equal
     }
 }
