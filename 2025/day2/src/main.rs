6db47ec16bc290367d9d51986ea99625aa30d43c
cj <curtis.longoria@gmail.com>
2025-12-27 22:57:38 -0600

    day 2 refactor

--- 2025/day2/src/main.rs
+++ 2025/day2/src/main.rs
@@ -1,5 +1,35 @@
+use clap::Parser;
+use clap::ValueEnum;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    mode: Mode,
+
+    #[arg(short, long)]
+    part: Part,
+}
+
+#[derive(ValueEnum, Clone)]
+enum Mode {
+    Test,
+    Real,
+}
+
+#[derive(ValueEnum, Clone)]
+enum Part {
+    #[value(name = "1")]
+    One,
+    #[value(name = "2")]
+    Two,
+}
+
 fn main() {
-    let data = "8284583-8497825,7171599589-7171806875,726-1031,109709-251143,1039-2064,650391-673817,674522-857785,53851-79525,8874170-8908147,4197684-4326484,22095-51217,92761-107689,23127451-23279882,4145708930-4145757240,375283-509798,585093-612147,7921-11457,899998-1044449,3-19,35-64,244-657,5514-7852,9292905274-9292965269,287261640-287314275,70-129,86249864-86269107,5441357-5687039,2493-5147,93835572-94041507,277109-336732,74668271-74836119,616692-643777,521461-548256,3131219357-3131417388";
+    let args = Args::parse();
+    let data = match args.mode {
+        Mode::Test => include_str!("../data/test"),
+        Mode::Real => include_str!("../data/data"),
+    };
 
     let res = data.split(",").fold(0, |acc, range| {
         let (start, end) = range.split_once("-").unwrap();
@@ -17,8 +47,10 @@ fn main() {
         let upper_int: usize = upper.parse().unwrap();
         for id_int in lower_int..=upper_int {
             let id = id_int.to_string();
-            // part1(&id, &mut total);
-            part2(&id, &mut total);
+            match args.part {
+                Part::One => part1(&id, &mut total),
+                Part::Two => part2(&id, &mut total),
+            }
         }
     }
     println!("{total}");
